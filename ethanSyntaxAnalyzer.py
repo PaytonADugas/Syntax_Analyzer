9dee187b1e34021bc10ece0cd1db8d309364ac72
Ethan Vaughan <ethandvaughan@gmail.com>
2021-09-20 17:22:13 -0700

    Create vars, main func

--- ethanSyntaxAnalyzer.py
+++ ethanSyntaxAnalyzer.py
@@ -6,4 +6,25 @@ token
 nextToken
 
 # Funcs
+def addChar()
+def getChar()
+def getNonBlank()
+
+# Char Classes
+LETTER = 0
+DIGIT = 1
+UNKNOWN = 99
+
+# Token Codes
+INT_LIT = 10
+IDENT = 11
+ASSIGN_OP = 20
+ADD_OP = 21
+SUB_OP = 22
+MULT_OP = 23
+DIV_OP = 24
+LEFT_PAREN = 25
+RIGHT_PAREN = 26
+
+def main():
 
