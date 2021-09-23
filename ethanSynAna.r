f134e2d6cdc0c79a6a51d416710b1d08a38a7683
Ethan Vaughan <ethandvaughan@gmail.com>
2021-09-22 18:40:47 -0700

    Fine tuning, working on separating '(' from 'sum'

--- ethanSynAna.r
+++ ethanSynAna.r
@@ -15,24 +15,26 @@ LEFT_PAREN <- 25
 RIGHT_PAREN <- 26
 
 # Open File as String
-data = readLines(file.choose('inFile.txt'), warn = FALSE)
+data = readLines(file.choose(), warn = FALSE)
 data = toString(data)
 
 # Split string into a list of characters
-listData <- strsplit(data, "")[[1]]
+listData <- strsplit(data, " ")[[1]]
 
 # Main Driver
 main <- function() {
-    if(data == null) {
-        print("ERROR - cannot open inFile.txt")
-    }   else {
-        getChar()
-    }  
+  if (is.null(data)) {
+    print("ERROR - cannot open inFile.txt")
+  } else {
+    for (i in seq.int(1, length(listData), 1)) {
+      getChar(listData[i])
+    }
+  }
 }
 
 # Lookup - looks up opperators and parentheses and returns the token
 lookup <- function(char) {
-    token <- switch(
+  token <- switch(
         char,
         "(" = LEFT_PAREN,
         ")" = RIGHT_PAREN,
@@ -41,51 +43,54 @@ lookup <- function(char) {
         "*" = MULT_OP,
         "/" = DIV_OP
     )
-    return (token)
+  return(token)
 }
 
 # isAlpha - checks if the character is a letter
 isAlpha <- function(char) {
-    !grepl("[^A-Za-z]", char)
+  return(!grepl("[^A-Za-z]", char))
 }
 
 # isDigit - checks if the character is a digit
 isDigit <- function(char) {
-    !grepl("\\D", char)
+  return(!grepl("\\D", char))
 }
 
 # getChar - get the next character of input and determine its character class
-getChar <- function() {
-    word <- ""
-    i <- 1  
-    for (i < length(listData)) {
-        if (listData[i] != " ") {
-            if (isAlpha(listData[i])) {
-                j = i 
-                while(isAlpha(listData[j]) {
-                paste(word, listData[j], sep = "")
-                }
-                lex(word, LETTER)
-                word = ""
-            }   else if (isdigit(listData[i])) {
-                lex(listData[i], DIGIT)
-            }   else {
-                lex(listData[i], UNKNOWN)
-            }
-        }
-        i = i + 1
+getChar <- function(char) {
+  if (nchar(char) > 1) {
+    tempStr <- ""
+    listChar <- strsplit(char, "")
+    for (c in listChar) {
+      if ((!isAlpha(c)) || !isDigit(c)) {
+        getChar(c)
+      } else {
+        paste(tempStr, c, sep = "")
+      }
     }
+    getChar(tempStr)
+  }
+  if (char != " ") {
+    if (isAlpha(char)) {
+      lex(char, LETTER)
+    }
+    else if (isDigit(char)) {
+      lex(char, DIGIT)
+    } else {
+      lex(char, UNKNOWN)
+    }
+  }
 }
 
 # lex - simple lexical analyzer for arithematic expressions
 lex <- function(char, class) {
-    if (class == LETTER) {
-        print(paste("Next token is:",LETTER,"Next lexeme is:",char, sep = " "))
-    }   else if (class == DIGIT) {
-        print(paste("Next token is:",DIGIT,"Next lexeme is:",char, sep = " "))
-    }   else {
-        print(paste("Next token is:",lookup(char),"Next lexeme is:",char, sep = " "))
-    }
+  if (class == LETTER) {
+    print(paste("Next token is:", LETTER, "Next lexeme is:", char, sep = " "))
+  } else if (class == DIGIT) {
+    print(paste("Next token is:", DIGIT, "Next lexeme is:", char, sep = " "))
+  } else {
+    print(paste("Next token is:", lookup(char), "Next lexeme is:", char, sep = " "))
+  }
 }
 
-main
+main()
